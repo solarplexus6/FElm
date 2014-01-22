2cc4d7707ec46a39f930b2bf18528a9b71e1b974
Filip Pawlak <filip.pawlak@gmail.com>
2014-01-22 11:45:11 +0100

    binopToFunction

--- Interpreter/Interpreter.fs
+++ Interpreter/Interpreter.fs
@@ -1,4 +1,19 @@
-﻿namespace Interpreter
+﻿module Interpreter
 
-type Class1() = 
-    member this.X = "F#"
+open System
+open AbstractSyntax
+
+let binopToFunction =
+  let convertOperator op =
+    fun x y -> (op x y : bool) |> Convert.ToInt32
+  in function
+  | Add -> (+)
+  | Sub -> (-)
+  | Mul -> (*)
+  | Div -> (/)
+  | Lt  -> convertOperator (<)
+  | Le  -> convertOperator (<=)
+  | Eq  -> convertOperator (=)
+  | Neq -> convertOperator (<>)
+  | Ge  -> convertOperator (>=)
+  | Gt  -> convertOperator (>)
