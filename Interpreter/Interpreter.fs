9ea6abcfe4bc2b1e2d1283fb7c261f6d221e09db
Filip Pawlak <filip.pawlak@gmail.com>
2014-01-22 15:23:19 +0100

    podstawienie

--- Interpreter/Interpreter.fs
+++ Interpreter/Interpreter.fs
@@ -2,6 +2,7 @@
 
 open System
 open AbstractSyntax
+open Gensym
 
 type context = EmptyContext
              | App1 of context * expr
@@ -30,3 +31,28 @@ let binopToFunction =
   | Neq -> convertOperator (<>)
   | Ge  -> convertOperator (>=)
   | Gt  -> convertOperator (>)
+
+let rec subst x e =
+  let rec subst' expr =
+    match expr with
+      | Var y when y = x -> e
+      | Var y when y <> x -> expr
+      | Fun (y, _) when y = x -> expr
+      | Fun (y, body) when y <> x ->
+        let freshVar = Gensym.next()
+        let body' = subst y (Var freshVar) body
+        let body'' = subst' body'
+        in Fun (freshVar, body'')
+      | App (e1, e2) -> App (subst' e1, subst' e2)
+      | Op (e1, op, e2) -> Op (subst' e1, op, subst' e2)
+      | If (e1, e2, e3) -> If (subst' e1, subst' e2, subst' e3)
+      | Let (y, e1, e2) when y = x -> Let (y, subst' e1, e2)
+      | Let (y, e1, e2) when y <> x ->
+        let freshVar = Gensym.next()
+        let e2' = subst y (Var freshVar) e2
+        let e2'' = subst' e2'
+        in Let (freshVar, subst' e1, e2'')
+      | Lift (e1, elist) -> Lift (subst' e1, List.map subst' elist)
+      | Foldp (e1, e2, e3) -> Foldp (subst' e1, subst' e2, subst' e3)
+      | _ -> expr
+  in subst'
