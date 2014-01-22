0e1c6de897b6c27507e8b2e41142cbeaf664ca73
Filip Pawlak <filip.pawlak@gmail.com>
2014-01-22 14:34:43 +0100

    definicja kontekstu ewaluacyjnego

--- Interpreter/Interpreter.fs
+++ Interpreter/Interpreter.fs
@@ -3,6 +3,19 @@
 open System
 open AbstractSyntax
 
+type context = EmptyContext
+             | App1 of context * expr
+             | Op1 of context * binop * expr
+             | Op2 of expr * binop * context
+             | If1 of context * expr * expr
+             | Let1 of varname * context * expr
+             | Let2 of varname * expr * context
+             | Lift1 of context * expr list
+             | Liftk of expr * expr list * context * expr list
+             | Foldp1 of context * expr * expr
+             | Foldp2 of expr * context * expr
+             | Foldp3 of expr * expr * context
+
 let binopToFunction =
   let convertOperator op =
     fun x y -> (op x y : bool) |> Convert.ToInt32
