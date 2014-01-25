fc249c2c3b8ed495460d3c0a260ce84fc911ec67
RafaŁ Łukaszewski <solarplexus6@gmail.com>
2014-01-25 18:11:39 +0100

    Poprawne pliki solucji i projektow;projekt testow
    Użycie xUnit i Unquote - wstępnie działają na vs2012.

---
+++ Tests/VerifyXunitUnquote.fs
@@ -0,0 +1,13 @@
+﻿module VerifyXunitUnquote
+
+open Swensen.Unquote
+open Xunit
+
+//should fail without exception
+[<Fact>]
+let ``test xunit support, this should fail with clean stack trace`` () =
+    test <@ 22 + 2 = 5 @>
+
+[<Fact>]
+let ``test xunit support, this should pass`` () =
+    test <@ 22 - 17 = 5 @>
