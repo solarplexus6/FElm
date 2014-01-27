12ce6294b448ac35e16262e847dda51511fbfbe0
RafaŁ Łukaszewski <solarplexus6@gmail.com>
2014-01-27 02:32:22 +0100

    Testy dla budowania grafu

--- Parser/ParserInterface.fs
+++ Parser/ParserInterface.fs
@@ -3,9 +3,11 @@
 open Parser
 open Lexer
 
-exception ParseException of string
+type ParseException (message:string, ?innerException:exn) =
+    inherit System.Exception (message, 
+        match innerException with | Some(ex) -> ex | _ -> null)
 
 let parse lexbuf = 
     try Parser.program Lexer.tokenize lexbuf with
         ex -> let pos = lexbuf.EndPos
-              in raise (ParseException (System.String.Format ("Parse exception at line: {0}, column: {1}", pos.Line, pos.Column)))
+              in raise (new ParseException (System.String.Format ("Parse exception at line: {0}, column: {1}", pos.Line, pos.Column), ex))
--- Tests/Interpreter/FunctionalXunit.fs
+++ Tests/Interpreter/FunctionalXunit.fs
@@ -9,8 +9,41 @@ open Xunit
 
 [<Fact>]
 let TestFix () =
-    test <@ Functional.normalize <| ParserInterface.parse (filenameToLexbuf "..\\..\\Interpreter\\fix.felm") = Num 120 @>
+    let lexbuf = ParserInterface.parse (filenameToLexbuf "..\\..\\Interpreter\\fix.felm") in
+    test <@ Functional.normalize lexbuf = Num 120 @>
 
 [<Fact>]
 let TestList () =
-    test <@ Functional.normalize <| ParserInterface.parse (filenameToLexbuf "..\\..\\Interpreter\\list.felm") = Num 5 @>
+    let lexbuf = ParserInterface.parse (filenameToLexbuf "..\\..\\Interpreter\\list.felm") in
+    test <@ Functional.normalize lexbuf = Num 5 @>
+
+[<Fact>]
+let TestLiftPath () = 
+    let lexbuf = ParserInterface.parse (filenameToLexbuf "..\\..\\Interpreter\\graphLiftPath.felm") in
+    test <@ Functional.normalize lexbuf = 
+        Let ("timesTwo",
+             Lift (Fun ("x",Op (Var "x",Mul,Num 2)),[Var "Signal.x"]), 
+             Let ("plusThree",Lift (Fun ("x",Op (Var "x",Add,Num 3)),[Var "timesTwo"]), 
+                 Lift (Fun ("y",Var "y"),[Var "plusThree"]))) @>
+
+[<Fact>]
+let TestLiftBranch () = 
+    let lexbuf = ParserInterface.parse (filenameToLexbuf "..\\..\\Interpreter\\graphLiftBranch.felm") in
+    test <@ Functional.normalize lexbuf = 
+        Let ("timesTwo",
+             Lift (Fun ("x",Op (Var "x",Mul,Num 2)),[Var "Signal.x"]),
+             Let ("plusThree",Lift (Fun ("x",Op (Var "x",Add,Num 3)),[Var "timesTwo"]),
+              Let ("minusFive",Lift (Fun ("z",Op (Var "z",Sub,Num 5)),[Var "timesTwo"]),
+                 Lift (Fun ("x",Fun ("y",Op (Var "x",Add,Var "y"))),
+                    [Var "plusThree"; Var "minusFive"])))) @>
+
+[<Fact>]
+let TestWithCount () = 
+    let lexbuf = ParserInterface.parse (filenameToLexbuf "..\\..\\Interpreter\\withCount.felm") in
+    test <@ match Functional.normalize lexbuf with
+                | Lift (Fun ("x",Fun ("y",Fun ("f",App (App (Var "f",Var "x"),Var "y")))),
+                        [Var "Signal.x";
+                         Foldp
+                            (Fun (_,Fun (_,Op (Var _,Add,Var _))),Num 0,Var "Signal.x")])
+                    -> true
+                | _ -> false @>
